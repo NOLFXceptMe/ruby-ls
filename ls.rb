64ed7cb528424b853c90267bb8ded99e8b33757e
Naveen Kumar Molleti <nerd.naveen@gmail.com>
2018-06-16 23:03:22 +0530

    Rudimentary support for -l

--- ls.rb
+++ ls.rb
@@ -2,6 +2,8 @@
 #
 # ls [-a] [file...]
 
+require 'etc'
+
 options = ARGV.select {|arg| arg.start_with?('-')}
 files = ARGV.select {|arg| !arg.start_with?('-')}
 
@@ -15,11 +17,54 @@ def filter_folder(file, opts)
   if File.directory? file
     Dir.entries(file)
         .select {|filename| !filename.start_with?(".") || opts.include?('-a')}
+        .map {|filename| File.join(file, filename)}
   else
     [file]
   end
 end
 
+def format_mode(mode)
+  mode
+end
+
+def get_user(uid)
+  Etc.getpwuid(uid).name
+end
+
+def get_group(gid)
+  Etc.getgrgid(gid).name
+end
+
+def format_mtime(mtime)
+  if(mtime > (Time.now - 6 * 30 * 24 * 60 * 60))
+    mtime.strftime "%b %e %H:%M"
+  else
+    mtime.strftime "%b %e %Y"
+  end
+end
+
+def long_list(file)
+  stat = File.stat(file)
+
+  "%s %s %s %s %d %s %s\n" %
+    [format_mode(stat.mode),
+     stat.nlink,
+     get_user(stat.uid),
+     get_group(stat.gid),
+     stat.size,
+     format_mtime(stat.mtime),
+     File.basename(file)]
+end
+
+def format_file(file, opts)
+  if opts.include?('-l')
+    long_list(file)
+  else
+    file
+  end
+end
+
 (files.empty? ? ["."] : files)
     .flat_map {|file| filter_folder(file, options)}
+    .map {|file| format_file(file, options)}
     .each {|filename| puts filename}
